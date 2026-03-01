80244ada6a3a08f283d398b78090a2bfe975cfe2
artem-idb <clojurelaba@gmail.com>
2026-03-01 21:08:00 +0300

    add desc func, upd data n README

--- src/laba_12/core.clj
+++ src/laba_12/core.clj
@@ -8,10 +8,14 @@
  :key-fn keyword)))
 
 (defn printJson [data]
-(doseq [i data] (println (format "%-20s age: %d salary: %d"
-(:name i)(int (:age i))(int (:salary i))))))
+(doseq [i data] (println (format "%-20s age: %d grade: %d"
+(:name i)(int (:age i))(int (:grade i))))))
+
+(defn descFilter [data field]
+(sort-by field #(compare %2 %1) data))
 
 (defn -main [& args]
-  (let [data (parseJson "resources/data.json")]
-    (println "\nВывод")
-    (printJson data)))
+(let [data (parseJson "resources/data.json")
+  sorted (descFilter data :grade)]
+  (println "\nВывод")
+  (printJson sorted)))
