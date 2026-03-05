4eb2747a4916087d47349dabff9f6e15ef9a6b06
artem-idb <clojurelaba@gmail.com>
2026-03-05 19:02:44 +0300

    validate input + combined all functions /w json into one func

--- src/laba_12/core.clj
+++ src/laba_12/core.clj
@@ -3,27 +3,28 @@
   (:require [clojure.data.json :as json]
   [clojure.java.io :as io]))
 
-(defn parseJson [filepath]
-(with-open [reader (io/reader filepath)] (json/read reader
- :key-fn keyword)))
+(defn processJson [filepath field desc?]
+(with-open [reader (io/reader filepath)]
+  (let [data (json/read reader :key-fn keyword)]
+  (if desc?
+    (sort-by field #(compare %2 %1) data)
+    (sort-by field #(compare %1 %2) data)))))
 
 (defn printJson [data]
 (doseq [i data] (println (format "%-20s age: %d grade: %d"
 (:name i)(int (:age i))(int (:grade i))))))
 
-(defn descFilter [data field]
-(sort-by field #(compare %2 %1) data))
-
-(defn ascFilter [data field]
-(sort-by field #(compare %1 %2) data))
+(defn readValidInput []
+  (loop [] (print "Ваш выбор: ") (flush)
+  (let [input (clojure.string/trim (read-line))]
+  (if (contains? #{"0" "1"} input)
+    input
+  (do (println "Ошибка: введите 0 или 1") (recur))))))
 
 (defn -main [& args]
-(let [data (parseJson "resources/data.json")]
   (println "Выберите сортировку:")
-  (println "  1 — по убыванию  (grade)")
-  (println "  0 — по возрастанию (grade)")
-  (print "Ваш выбор: ")
-  (flush)
-(let [input (read-line) sorted (if (= input "1")  (descFilter data :grade) (ascFilter data :grade)) ]
-  (println (if (= input "1") "\nСортировка по убыванию:" "\nСортировка по возрастанию:"))
-  (printJson sorted))))
+  (println "  1  по убыванию  (grade)")
+  (println "  0  по возрастанию (grade)")
+(let [input  (readValidInput) desc?  (= input "1") sorted (processJson "resources/data.json" :grade desc?)]
+  (println (if desc? "\nСортировка по убыванию:" "\nСортировка по возрастанию:"))
+  (printJson sorted)))
